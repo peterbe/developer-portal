c04e947baf426b673f0010067c70e9d28046b472
rjohnson <rjohnson@mozilla.com>
2019-07-26 16:16:31 -0700

    increase workers and replicas

--- k8s/config/stage.sh
+++ k8s/config/stage.sh
@@ -13,12 +13,12 @@ alias kc="kubectl -n ${K8S_NAMESPACE}"
 export APP_SERVICE_CERT_ARN=arn:aws:acm:us-west-2:178589013767:certificate/ccc148ae-2a73-4551-84d4-f785f0d5e67e
 export APP_BUCKET_ROLE_ARN=arn:aws:iam::178589013767:role/developer-portal-stage-us-west-2-role
 
-export APP_REPLICAS=2
+export APP_REPLICAS=5
 export APP_CPU_LIMIT=2
 export APP_CPU_REQUEST=500m
 export APP_MEMORY_LIMIT=4Gi
 export APP_MEMORY_REQUEST=2Gi
-export APP_GUNICORN_WORKERS=2
+export APP_GUNICORN_WORKERS=4
 export APP_HOST=developer-portal-stage.mdn.mozit.cloud
 export APP_AWS_BUCKET_NAME=developer-portal-stage-178589013767
 export APP_AWS_BUCKET_REGION=us-west-2
